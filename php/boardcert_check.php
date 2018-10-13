e853654205db0b2c2fddb8712d510577041d0d0e
Nikhil Goyal <subs@spandan.com>
2018-10-12 22:16:23 -0400

    Verification code added

--- php/boardcert_check.php
+++ php/boardcert_check.php
@@ -18,12 +18,12 @@ function board_check($type, $init, $recert) {
 		case "R":
 			if (($recert+0)===0) {$rVal['code']=1; $rVal['error']="No recert year listed";}
 			else if (($thisyear - $recert) > 10) {$rVal['code']=1; $rVal['error']="Recert possibly expired"; }
-			else {$rVal['code']=2; $rVal['error']="Should this be 'M' (participating in MOC)?";}
+			else {$rVal['code']=2; $rVal['error']="Should this be 'M' or 'C' (participating in MOC/OCC)?";}
 			return $rVal;
 			break;
 			
 		case "O":
-			if (($thisyear - $init) > 10) {$rVal['code']=1; $rVal['error']="Original cert possibly expired. Use 'N' if time-unlimited certification"; }
+			if (($thisyear - $init) > 10) {$rVal['code']=1; $rVal['error']="Original cert possibly expired. Check if recertified or time-unlimited certification"; }
 			return $rVal;
 			break;
 			
@@ -38,6 +38,7 @@ function board_check($type, $init, $recert) {
 			break;
 			
 		case "M":
+		case "C":
 			return $rVal;
 			break;
 			
@@ -56,58 +57,99 @@ $rVal=array(
 	"results" => array ()
 );
 
+$_POST['boardblock']= <<<BB
+Physician Faculty Roster
+
+List alphabetically and by site all physician faculty who have a significant role (teaching or mentoring) in the education of residents/fellows and who have documented qualifications to instruct and supervise. List the Program Director first.
+
+Core physician faculty must:
+ Devote sufficient time to the educational program to fulfill their supervisory and teaching responsibilities and demonstrate a strong interest in resident education
+ Administer and maintain an educational environment conducive to educating residents in each of the ACGME competency areas
+ Participate in faculty development programs designed to enhance the effectiveness of their teaching and to promote scholarly activity
+ Establish and maintain an environment of inquiry and scholarship with an active research component
+ Regularly participate in organized clinical discussions, rounds, journal clubs, and conferences
+ Encourage and support residents in pursuing scholarly activities
+ Be clinically active
+ Devote the majority of their professional efforts to the program
+All physicians who devote at least 15 hours per week to resident education and administration are designated as core faculty.
+
+All core physician faculty should teach and advise residents as well as participate in at least 1 of the following:
+ Evaluate the competency domains
+ Work closely with and support the program director
+ Assist in developing and implementing evaluation systems
+Program directors will not be designated as core faculty.
+
+Continued Accreditation programs: A CV is only required for the program director.
+
+New Applications and Initial Accreditation programs: A CV is required for the program director and each active physician faculty member on your roster.
+
+
+Name	Core Faculty	Based Mainly at Inst. #	Specialties / Certifications	No. of Years Teaching in This Specialty	Average Hours Per Week Spent On
+Specialty / Certification	Cert	Original Cert Year	Cert Status	Re-cert Year	Clinical Supervision	Admin	Didactic Teaching	Research
+Nikhil Goyal, MD
+(Program Director, Transitional Year Residency Program)	N	1	Emergency medicine	ABMS	2007	R	2018	9	20	15	9	1
+Clinical informatics	ABMS	2016	O	--
+Internal medicine	ABMS	2006	M	2016
+Odaliz Abreu-Lanfranco, MD
+(Program Director, Internal Medicine Residency)	Y	1	Internal medicine	ABMS	2006	M	2016	1	10	27	6	1
+Infectious disease	ABMS	2012	N	--
+Bradley Jaskulka, MD
+(Associate Program Director, Transitional Year Residency)	Y	1	Emergency medicine	ABMS	2008	M	--	3	20	10	2	1
+Sports medicine	ABMS	2009	M	--
+Vinay Shah, MD
+(Internal Medicine Faculty, CCC & PEC Member)	Y	1	Internal medicine	ABMS	1997	M	2017	5	20	1	1	1
+Taher Vohra, MD
+(Emergency Medicine Residency Program Director)	Y	1	Emergency medicine	ABMS	2006	M	2015	4	20	1	2	1
+Certification Status:
+Certification in the primary specialty refers to Board Certification. Certification for the secondary specialty refers to sub-board certification. If the secondary specialty is a core ACGME specialty (e.g., Internal Medicine, Pediatrics, etc.), the certification question refers to Board Certification.
+
+R = Re-Certified
+O = Time Limited Certificate/Original Certification Currently Valid
+L = Certification Lapsed
+N = Time-unlimited certificate/no Re-Certification
+M = Meets MOC/CC Requirements
+C = Meets Osteopathic Continuous Certification (OCC)
+Based Mainly at Institution #:
+1 = [250331] Henry Ford Hospital
+*=Institution is an elective rotation site.
+**=Institution not on list of active participating sites.
+Educational Focus:
+† = Program and Osteopathic Faculty
+† † = Osteopathic Faculty
+BB;
+
 if ((!isset($_POST['boardblock'])) || (strlen($_POST['boardblock'])<10)) err(1, "Missing board text block");
 
-// this one splits up the table into one row per faculty
-$split=preg_split('/([\w-]+ \w+),.+\s+\([^\)]+\)\D*\d\s+(\D*)(\d{4})\s+([ROLNM])(\s+-?\s*(?:\d{4})?)(?:\s*\d{1,2}){5}\s*/', $_POST['boardblock'], 0, PREG_SPLIT_DELIM_CAPTURE);
-if (count($split)===1) err(2, "Preg_split failed");
-//print_r($split); 
+// extract all info from boardblock
+preg_match_all('/(([\w -]+), .*[\r\n]+\D+\d{1,2}\t)?([\w ]+)\t(ABMS|AOA)\s+(\d{4})\s+([ROLNMC])\s+(--|\d{4})/m', $_POST['boardblock'], $matches, PREG_SET_ORDER);
 
-$j=0;
-for ($i=1; isset($split[$i]); $i+=6) {
-	$match[$j]['name']=$split[$i];
-	$match[$j]['specialty']=trim(preg_replace("/\s/", " ", $split[$i+1]));
-	$match[$j]['init_cert']=$split[$i+2];
-	$match[$j]['type']=$split[$i+3];
-	$match[$j]['recert']=trim($split[$i+4]);
-	$match[$j]['remainder']=trim($split[$i+5]); 	// the remainder of the text on this row may have secondary board info
-
-	// look in remainder text for second board
-	$matches=array();
-	$ret=preg_match('/(\D+)(\d{4})\s+([ROLNM])(\s+-?\s*(?:\d{4})?)/',$split[$i+5],$matches);
-	if ($ret===1) {
-		$match[$j]['sec_specialty']=trim(preg_replace("/\s/", " ", $matches[1]));
-		$match[$j]['sec_init_cert']=$matches[2];
-		$match[$j]['sec_type']=$matches[3];
-		$match[$j]['sec_recert']=$matches[4];
+$boardcerts=array();
+foreach($matches as $match) {
+	if ($match[2]!='') {
+		// new faculty
+		$bc=array();
+		$bc['name']=$match[2];
 	}
-	$j++;
+	$bc['specialty']=$match[4]." ".$match[3];
+	$bc['orig_year']=$match[5];
+	$bc['status']=$match[6];
+	if ($match[7]==='--') $match[7]=null;
+	$bc['recert_year']=$match[7];
+	$boardcerts[]=$bc;
 }
 
-// print_r($match); exit;
-	
-$j=0;
-foreach($match as $i => $m) {
-	// look through cert type - R, O, L, N, M. First 3 are problematic. N or M is cool
-	$rVal['results'][$j]['name']=$m['name'];
-	$rVal['results'][$j]['specialty']=$m['specialty'];
-	$r=board_check($m['type'], $m['init_cert'], $m['recert']);
-	$rVal['results'][$j]['issues']=$r['code'];
-	if ($r['code']!==0) $rVal['results'][$j]['descr']=$r['error'];
-
-	if (isset($m['sec_type'])) {
-		// has a secondary board info - validate
-		$j++;
-		$rVal['results'][$j]['name']=$m['name'];
-		$rVal['results'][$j]['specialty']=$m['sec_specialty'];
-		$r=board_check($m['sec_type'], $m['sec_init_cert'], $m['sec_recert']);
-		$rVal['results'][$j]['issues']=$r['code'];
-		if ($r['code']!==0) $rVal['results'][$j]['descr']=$r['error'];
-	}
-	$j++;
+// verify board certifications
+foreach ($boardcerts as $bc) {
+	$r=array();
+	$check=board_check($bc['status'], $bc['orig_year'], $bc['recert_year']);
+	$r['name']=$bc['name'];
+	$r['specialty']=$bc['specialty'];
+	$r['issues']=$check['code'];
+	if ($check['code']!==0) $r['descr']=$check['error'];
+	$rVal['results'][]=$r;
 }
 
 header('Content-Type: application/json');
-echo json_encode($rVal);
+echo json_encode($rVal, JSON_PRETTY_PRINT);
 exit(0);
 ?>
