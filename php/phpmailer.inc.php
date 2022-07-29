b9fe4647340c921fab7cf642c762eb40c4f25179
3NS@HomeServer <info@trynomial.solutions>
2022-07-28 20:51:54 -0400

    move pwds to config

--- php/phpmailer.inc.php
+++ php/phpmailer.inc.php
@@ -5,19 +5,21 @@ use PHPMailer\PHPMailer\Exception;
 
 require __DIR__.'/../vendor/autoload.php';
 $phpmailer = new PHPMailer(true);
+if (!isset($cfg)) require_once __DIR__."/config.inc.php";
 
 try {
     //Server settings
     // $phpmailer->SMTPDebug = SMTP::DEBUG_SERVER;
     $phpmailer->isSMTP();
-    $phpmailer->Host       = 'secure.emailsrvr.com';
+    $phpmailer->Host       = $cfg['email_host'];
     $phpmailer->SMTPAuth   = true;
-    $phpmailer->Username   = 'noreply@mwconsulting.dev';
-    $phpmailer->Password   = 'a36L#Qo*yFn6oWpGzE';
+    $phpmailer->Username   = $cfg['email_user'];
+    $phpmailer->Password   = $cfg['email_pwd'];
     $phpmailer->SMTPSecure = 'tls';
     $phpmailer->Port       = 587;
     $phpmailer->addAddress("webmaster@meded.app");
     $phpmailer->Subject = "[AUSAM2 Error]";
+    $phpmailer->From        = 'webmaster@meded.app';
     $phpmailer->isHTML(false);
 } catch (Exception $e) {
     throw new RuntimeException("phpmailer init Error: {$phpmailer->ErrorInfo}");
